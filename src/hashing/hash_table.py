24bf7a1d571791444606b00b70720fe527b32681
gabriel <gabriel.goncalves@trilogo.com.br>
2018-02-03 17:02:27 -0300

    hash with open addressing initiated

--- src/hashing/hash_table.py
+++ src/hashing/hash_table.py
@@ -3,11 +3,11 @@ class HashTable:
         Basic Hash Table example with open addressing and linear probing
     """
 
-    def __init__(self, size_table):
+    def __init__(self, size_table, charge_factor=None):
         self.size_table = size_table
         self.values = [None] * self.size_table
         self.keys = [None] * self.size_table
-        self.charge_factor = 1
+        self.charge_factor = 1 if charge_factor is None else charge_factor
 
     def balanced_factor(self):
         return sum([1 for slot in self.values
@@ -17,9 +17,12 @@ class HashTable:
         return key % self.size_table
 
     def __check_prime(self, number):
+        """
+            it's not the best solution
+        """
         return all([number % i for i in range(2, number)])
 
-    def step_by_step(self, step_ord):
+    def __step_by_step(self, step_ord):
 
         print("step %s".format(step_ord))
         print([i for i in range(len(self.values))])
@@ -29,7 +32,7 @@ class HashTable:
         i = 1
         for value in values:
             self.insert_data(value)
-            self.step_by_step(i)
+            self.__step_by_step(i)
             i += 1
 
     def double_next_prime(self):
@@ -74,4 +77,6 @@ class HashTable:
                 self.__set_value(self.colision_resolution(key), data)
             else:
                 self.rehashing()
+    
+
 
