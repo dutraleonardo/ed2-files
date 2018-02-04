01bddecffb43bc95072bc310d252dffeae183522
gabriel <gabriel.goncalves@trilogo.com.br>
2018-02-04 16:49:48 -0300

    added Quadratic probing

--- src/hashing/hash_table.py
+++ src/hashing/hash_table.py
@@ -3,17 +3,22 @@ class HashTable:
         Basic Hash Table example with open addressing and linear probing
     """
 
-    def __init__(self, size_table, charge_factor=None):
+    def __init__(self, size_table, charge_factor=None, lim_charge=None):
         self.size_table = size_table
         self.values = [None] * self.size_table
-        self.keys = [None] * self.size_table
+        self.lim_charge = 0.75 if lim_charge is None else lim_charge
         self.charge_factor = 1 if charge_factor is None else charge_factor
+        self.__aux_list = []
+        self.__keys = {}
+
+    def keys(self):
+        return self.__keys
 
     def balanced_factor(self):
         return sum([1 for slot in self.values
                     if slot is not None]) / (self.size_table * self.charge_factor)
 
-    def __hash_function(self, key):
+    def hash_function(self, key):
         return key % self.size_table
 
     def __check_prime(self, number):
@@ -30,6 +35,7 @@ class HashTable:
 
     def bulk_insert(self, values):
         i = 1
+        self.__aux_list = values
         for value in values:
             self.insert_data(value)
             self.__step_by_step(i)
@@ -46,31 +52,31 @@ class HashTable:
 
     def __set_value(self, key, data):
         self.values[key] = data
-        self.keys[key] = key
+        self.__keys[key] = data
 
     def colision_resolution(self, key):
-        new_key = self.__hash_function(key + 1)
+        new_key = self.hash_function(key + 1)
 
         while self.values[new_key] is not None \
                 and self.values[new_key] != key:
 
             if self.values.count(None) > 0:
-                new_key += 1
+                new_key = self.hash_function(new_key + 1)
             else:
                 new_key = None
                 break
+
         return new_key
 
     def rehashing(self):
         survivor_values = [value for value in self.values if value is not None]
-        print(survivor_values)
         self.size_table = self.double_next_prime()
-        self.keys = [None] * self.size_table
-        self.values = [None]*self.size_table #hell's pointers D: don't DRY ;/
-        list(map(self.insert_data, survivor_values))
+        self.__keys.clear()
+        self.values = [None] * self.size_table #hell's pointers D: don't DRY ;/
+        [self.insert_data(data) for data in survivor_values]
 
     def insert_data(self, data):
-        key = self.__hash_function(data)
+        key = self.hash_function(data)
 
         if self.values[key] is None:
             self.__set_value(key, data)
@@ -84,6 +90,6 @@ class HashTable:
                 self.__set_value(colision_resolution, data)
             else:
                 self.rehashing()
-    
+                self.insert_data(data)
 
 
---
+++ src/hashing/quadratic_probing.py
@@ -0,0 +1,24 @@
+from .hash_table import HashTable
+
+
+class QuadraticProbing(HashTable):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def colision_resolution(self, key):
+        i = 1
+        new_key = self.hash_function(key + i*i)
+
+        while self.values[new_key] is not None \
+                and self.values[new_key] != key:
+
+            if self.balanced_factor() >= self.lim_charge:
+                new_key = None
+                break
+
+            else:
+                i += 1
+                new_key = self.hash_function(key + i*i)
+
+        return new_key
